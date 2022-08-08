b3be7b9e1945a85edfb6217f4006e3aec5a7e3a7
Justin Handley <justin@pirateandfox.com>
2022-08-08 17:33:17 -0400

    Update nx and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './apps/**/*.html',
-      './apps/**/*.ts',
-      './apps/**/*.tsx',
-      './libs/**/*.html',
-      './libs/**/*.ts',
-      './libs/**/*.tsx',
-    ],
-  },
+  content: [
+    './apps/**/*.html',
+    './apps/**/*.ts',
+    './apps/**/*.tsx',
+    './libs/**/*.html',
+    './libs/**/*.ts',
+    './libs/**/*.tsx',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('flowbite/plugin')],
-  content: ['node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
 }
