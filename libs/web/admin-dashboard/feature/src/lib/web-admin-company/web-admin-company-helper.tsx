d24ee1c5038f2f6df88af5842271ec0da30412aa
Justin Handley <justin@pirateandfox.com>
2022-08-12 15:42:16 -0400

    Add members to company

--- libs/web/admin-dashboard/feature/src/lib/web-admin-company/web-admin-company-helper.tsx
+++ libs/web/admin-dashboard/feature/src/lib/web-admin-company/web-admin-company-helper.tsx
@@ -1,3 +1,12 @@
 import { WebUiFormField } from '@biztobiz/web-ui/form'
+import {AdminUsersDocument} from '@biztobiz/shared/util-sdk'
+import {mapUsers} from '../web-admin-helper/web-admin-helper'
 
-export const companyFields: WebUiFormField[] = [WebUiFormField.input('name', { label: 'Company Name', required: true })]
+export const companyFields: WebUiFormField[] = [
+  WebUiFormField.input('name', { label: 'Company Name', required: true }),
+  WebUiFormField.relationSelect('members', {
+    label: 'Members',
+    document: AdminUsersDocument,
+    selectOptionsFunction: mapUsers,
+    dataType: 'users',
+  }),]
