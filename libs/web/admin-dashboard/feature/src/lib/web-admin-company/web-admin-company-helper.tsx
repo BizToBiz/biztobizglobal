880b6bd9e7f745d98c00b449b6f70a7cee3bc1b3
Justin Handley <justin@pirateandfox.com>
2022-08-16 15:01:55 -0400

    Formatting

--- libs/web/admin-dashboard/feature/src/lib/web-admin-company/web-admin-company-helper.tsx
+++ libs/web/admin-dashboard/feature/src/lib/web-admin-company/web-admin-company-helper.tsx
@@ -1,6 +1,6 @@
 import { WebUiFormField } from '@biztobiz/web-ui/form'
-import {AdminUsersDocument} from '@biztobiz/shared/util-sdk'
-import {mapUsers} from '../web-admin-helper/web-admin-helper'
+import { AdminUsersDocument } from '@biztobiz/shared/util-sdk'
+import { mapUsers } from '../web-admin-helper/web-admin-helper'
 
 export const companyFields: WebUiFormField[] = [
   WebUiFormField.input('name', { label: 'Company Name', required: true }),
@@ -9,4 +9,5 @@ export const companyFields: WebUiFormField[] = [
     document: AdminUsersDocument,
     selectOptionsFunction: mapUsers,
     dataType: 'users',
-  }),]
+  }),
+]
