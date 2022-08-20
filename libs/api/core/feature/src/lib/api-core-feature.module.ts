8c75a2efe5034a5d6fa11f2db89497b50d6ecddc
Justin Handley <justin@pirateandfox.com>
2022-08-19 20:16:37 -0400

    Add static file serving

--- libs/api/core/feature/src/lib/api-core-feature.module.ts
+++ libs/api/core/feature/src/lib/api-core-feature.module.ts
@@ -1,16 +1,20 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { GraphQLIntercomModule } from '@kikstart-playground/graphql-intercom'
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { PubSub } from 'graphql-subscriptions'
 import { join } from 'path'
-
 import { configuration } from './config/configuration'
 import { validationSchema } from './config/validation'
 import { ApiCoreFeatureController } from './api-core-feature.controller'
 import { ApiCoreFeatureResolver } from './api-core-feature.resolver'
 import { ApiCoreFeatureService } from './api-core-feature.service'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { writeFileSync } from 'fs'
+import { ensureDirSync, existsSync } from 'fs-extra'
+
+const rootPath = join(__dirname, '..', 'biztobiz')
 
 @Module({
   imports: [
@@ -27,9 +31,21 @@ import { ApiCoreFeatureService } from './api-core-feature.service'
       sortSchema: true,
     }),
     GraphQLIntercomModule.forRoot({ pubSub: new PubSub() }),
+    ServeStaticModule.forRoot({
+      rootPath,
+      exclude: ['/api', '/graphql'],
+    }),
   ],
   controllers: [ApiCoreFeatureController],
   providers: [ApiCoreFeatureResolver, ApiCoreFeatureService],
   exports: [ApiCoreFeatureService],
 })
-export class ApiCoreFeatureModule {}
+export class ApiCoreFeatureModule {
+  constructor() {
+    if (!existsSync(rootPath)) {
+      ensureDirSync(rootPath)
+      writeFileSync(join(rootPath, 'index.html'), `<pre>Run 'yarn build:web' to build the frontend.</pre>`)
+      Logger.verbose(`Created static root path ${rootPath}`)
+    }
+  }
+}
